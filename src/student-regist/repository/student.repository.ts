a6d093a731c3f9d396c276879088bf8f6a781bd6
CJM <whdals9514@naver.com>
2023-04-25 18:25:56 +0900

    sms

--- src/student-regist/repository/student.repository.ts
+++ src/student-regist/repository/student.repository.ts
@@ -3,34 +3,88 @@ import { Student } from "src/entities/student.entity";
 import { DataSource, Repository } from "typeorm";
 import { StudentDto } from "../dto/student.dto";
 import { User } from "src/entities/user.entity";
-import { SNS } from 'aws-sdk';
 import { CheckDto } from "../dto/check.dto";
+import CryptoJS from 'crypto-js'
+import 'dotenv/config'
+
 
 @Injectable()
 export class StudentRepository extends Repository<Student> {
     private logger = new Logger();
-    private readonly sns = new SNS({
-        region: 'ap-northeast-1'
-    });
-    
+
     constructor(private dataSource: DataSource) {
         super(Student, dataSource.createEntityManager());
     }
 
     async goTo(checkDto: CheckDto): Promise<void> {
-        this.logger.debug(`${checkDto.companyName}'s student ${checkDto.myNumber} is goTo`)
+        // Find student Data
         const data = await this.getStudentBymyNumber(checkDto.myNumber)
-        // AWS SMS service
         this.logger.debug(data.parentNumber, `${data.userName}이 ${checkDto.companyName}에 등원했습니다.`, 'student Repository')
-        // this.sendSMS(data.parentNumber, `${data.userName}이 ${checkDto.companyName}에 등원했습니다.`)
+        // Naver SENS service for sms sending
+        if (data) {
+            return this.sendSMS(data.parentNumber, `${data.userName}이 ${checkDto.companyName}에 등원했습니다.`)
+        }
     }
 
-    async sendSMS(phoneNumber: string, message: string){
-        const msg = {
-            Message: message,
-            PhoneNumber: phoneNumber
+    async sendSMS(phoneNumber: string, message: string) {
+        const accessKey: string = process.env.NCLOUD_ACCESSKEY;
+        const secretKey: string = process.env.NCLOUD_SECRETKEY;
+        var timeStamp: string = Date.now().toString();
+
+        const signatureKey: string = this.makeSignature(accessKey, secretKey, timeStamp);
+
+        const sms = {
+            "type": "SMS",
+            "from": process.env.NCLOUD_SENDPHONENUMBER,
+            "content": "학원톡톡의 알림 서비스입니다.",
+            "messages": [
+                {
+                    "to": phoneNumber,
+                    "content": `${message}`
+                }
+            ],
         }
-        return this.sns.publish(msg).promise()
+
+        const serviceId: string = process.env.NCLOUD_SERVICEID;
+        const SENSURL: string = `https://sens.apigw.ntruss.com/sms/v2/services/${serviceId}/messages`;
+        const res = await fetch(SENSURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-ncp-apigw-timestamp': timeStamp,
+                'x-ncp-iam-access-key': 'hk0m0O3HxNQOOaCo0jxj',
+                'x-ncp-apigw-signature-v2': signatureKey
+            },
+            body: JSON.stringify(sms)
+        })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
+    makeSignature(accesssKey: string, secretKey: string, timeStamp: string): string {
+        var space = " ";				// one space
+        var newLine = "\n";				// new line
+        var method = "GET";				// method
+        var url = "/sms/v2";	        // url (include query string)
+        var timestamp = timeStamp;			// current timestamp (epoch)
+        var accessKey = accesssKey;			// access key id (from portal or Sub Account)
+        var secretKey = secretKey;			// secret key (from portal or Sub Account)
+
+        var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+        hmac.update(method);
+        hmac.update(space);
+        hmac.update(url);
+        hmac.update(newLine);
+        hmac.update(timestamp);
+        hmac.update(newLine);
+        hmac.update(accessKey);
+
+        var hash = hmac.finalize();
+
+        const signatureKey = hash.toString(CryptoJS.enc.Base64)
+
+        this.logger.debug(`${signatureKey}`, 'student Repository');
+        return signatureKey;
     }
 
     // Make CRUD for Student
@@ -79,7 +133,7 @@ export class StudentRepository extends Repository<Student> {
         try {
             await this.save(student);
         } catch (error) {
-            this.logger.debug(`${userName} update is failed!`,'Student-regist Reposiotry');
+            this.logger.debug(`${userName} update is failed!`, 'Student-regist Reposiotry');
             console.log(error.code);
         }
         return student;
@@ -92,7 +146,7 @@ export class StudentRepository extends Repository<Student> {
         try {
             await this.delete(student);
         } catch (error) {
-            this.logger.debug(`${userName} delete is failed!`,'Student-regist Reposiotry');
+            this.logger.debug(`${userName} delete is failed!`, 'Student-regist Reposiotry');
             console.log(error.code);
         }
     };
