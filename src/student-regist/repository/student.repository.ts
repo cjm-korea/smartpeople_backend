861c08cd5ccd0cc08fa6fbf242aa3f083166a676
CJM <whdals9514@naver.com>
2023-05-13 16:30:37 +0900

    sms

--- src/student-regist/repository/student.repository.ts
+++ src/student-regist/repository/student.repository.ts
@@ -4,8 +4,9 @@ import { DataSource, Repository } from "typeorm";
 import { StudentDto } from "../dto/student.dto";
 import { User } from "src/entities/user.entity";
 import { CheckDto } from "../dto/check.dto";
-// import * as CryptoJS from 'crypto-js'
 import 'dotenv/config'
+import * as CryptoJS from "crypto-js";
+import fetch from 'node-fetch';
 
 
 @Injectable()
@@ -22,18 +23,23 @@ export class StudentRepository extends Repository<Student> {
         this.logger.debug(data.parentNumber, `${data.userName}이 ${checkDto.companyName}에 등원했습니다.`, 'student Repository')
         // Naver SENS service for sms sending
         if (data) {
-            return this.sendSMS(data.parentNumber, `${data.userName}이 ${checkDto.companyName}에 등원했습니다.`)
+            this.sendSMS(data.parentNumber, `${data.userName}이 ${checkDto.companyName}에 등원했습니다.`)
         }else{
             new NotFoundException(`Can't find ${checkDto.myNumber} in Dataset`)
         }
     }
 
+    // Depend on goTo
     async sendSMS(phoneNumber: string, message: string) {
         const accessKey: string = process.env.NCLOUD_ACCESSKEY;
         const secretKey: string = process.env.NCLOUD_SECRETKEY;
         var timeStamp: string = Date.now().toString();
+        this.logger.debug(`${accessKey} + ${secretKey} + ${timeStamp}`, 'StudentRepository');
 
         const signatureKey: string = this.makeSignature(accessKey, secretKey, timeStamp);
+
+        var sendPhoneNumber = phoneNumber.replace(/-/g, '');
+        this.logger.debug(`${sendPhoneNumber} is send to number`, 'StudentRepository');
 
         const sms = {
             "type": "SMS",
@@ -41,8 +47,8 @@ export class StudentRepository extends Repository<Student> {
             "content": "학원톡톡의 알림 서비스입니다.",
             "messages": [
                 {
-                    "to": phoneNumber,
-                    "content": `${message}`
+                    "to": sendPhoneNumber,
+                    "content": `${message}` 
                 }
             ],
         }
@@ -52,9 +58,9 @@ export class StudentRepository extends Repository<Student> {
         const res = await fetch(SENSURL, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json; charset=utf-8',
                 'x-ncp-apigw-timestamp': timeStamp,
-                'x-ncp-iam-access-key': 'hk0m0O3HxNQOOaCo0jxj',
+                'x-ncp-iam-access-key': process.env.NCLOUD_ACCESSKEY,
                 'x-ncp-apigw-signature-v2': signatureKey
             },
             body: JSON.stringify(sms)
@@ -63,11 +69,13 @@ export class StudentRepository extends Repository<Student> {
             .catch(err => console.log(err))
     }
 
+    // Depend on goTo
     makeSignature(accesssKey: string, secretKey: string, timeStamp: string): string {
         var space = " ";				// one space
         var newLine = "\n";				// new line
-        var method = "GET";				// method
-        var url = "/sms/v2";	        // url (include query string)
+        var method = "POST";			// method
+        var serviceId = process.env.NCLOUD_SERVICEID;
+        var url = `/sms/v2/services/${serviceId}/messages`;	        // url (include query string)
         var timestamp = timeStamp;		// current timestamp (epoch)
         var accessKey = accesssKey;		// access key id (from portal or Sub Account)
         var secretKey = secretKey;		// secret key (from portal or Sub Account)
@@ -83,7 +91,7 @@ export class StudentRepository extends Repository<Student> {
 
         var hash = hmac.finalize();
 
-        const signatureKey: string = hash.toString(CryptoJS.enc.Base64)
+        var signatureKey: string = hash.toString(CryptoJS.enc.Base64)
 
         this.logger.debug(`${signatureKey}`, 'student Repository');
         return signatureKey;
